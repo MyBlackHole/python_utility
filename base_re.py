c34a8ee62076ddc58aa6110cd6e05fba4cff258d
Black Hole <1358244533@qq.com>
2020-12-03 11:40:23 +0800

    re

--- base_re.py
+++ base_re.py
@@ -16,6 +16,8 @@ __author__ = 'Black Hole'
 
 import re
 
+from loguru import logger
+
 
 def is_chinese(_str: str) -> bool:
     """
@@ -47,6 +49,23 @@ def is_un_chinese(_str: str) -> bool:
     if chinese:
         return True
     return False
+
+
+def get_from(url: str) -> str:
+    """
+    获取 url 域名
+    Args:
+        url:  url
+    Input: http://www.baidu.com/
+    Returns: www.baidu.com
+    Class: <class 'str'>
+    """
+    try:
+        f = re.search('(http|https)://(.*?)/', url).group(2)
+        return f
+    except Exception as e:
+        logger.exception(e)
+        return ''
 
 
 if __name__ == '__main__':
